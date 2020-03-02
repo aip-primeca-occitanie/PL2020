5b6cff7d764e0d74242eda4b97540abf3c789eae
lucas-veit <veit.lucas67@gmail.com>
2020-03-02 12:49:33 +0100

    4main robots en 1

--- ros_ws/src/robots/src/main_robot.cpp
+++ ros_ws/src/robots/src/main_robot.cpp
@@ -2,23 +2,23 @@
 
 #include "Robot.h"
 
-#include <ros/ros.h>
-
 void ShutdownCallback(const std_msgs::Byte::ConstPtr& msg)
 {
-		ros::shutdown();
+	ros::shutdown();
 }
 
 int main(int argc, char **argv)
 {
 	//Initialisation du noeud ROS
-	ros::init(argc,argv,"robot3");
+	string name="robot";
+	name.append(argv[1]);
+	ros::init(argc,argv,name);
 	ros::NodeHandle nh;
 	ros::Subscriber sub_shutdown = nh.subscribe("/commande_locale/shutdown",10,&ShutdownCallback);
 
 	//CmdRobot
-	Robot robot3(3);
-	robot3.init(nh);
+	Robot robot(atoi(argv[1]));
+	robot.init(nh);
 
 	int compteur=0;
 	ros::Rate loop_rate(25);
@@ -26,10 +26,12 @@ int main(int argc, char **argv)
 	{
 		if(compteur++>25)
 		{
-			robot3.update();
+			robot.update();
 			compteur=0;
 		}
 		ros::spinOnce();
 		loop_rate.sleep();
+
+
 	}
 }
